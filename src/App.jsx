f6d0f315f38db7728cecb4bfd02bd04b0afa83df
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-11-14 19:59:20 +0200

    add bars icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="bg-blue-200 font-lora">Hello world</h1>
+      <h1 className="bg-red-300 font-lora">Hello world , how are you doing</h1>
+      <div>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
     </>
   );
 }
