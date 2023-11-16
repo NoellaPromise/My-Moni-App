0c3868b672df4f7ec81025bb4e404f7faa3ae1cd
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-11-16 11:09:18 +0200

    feat: add the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import BookCar from "./pages/BookCar";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/menu" element={<BookCar />} />
+        </Routes>
+      </Router>
     </>
   );
 }
